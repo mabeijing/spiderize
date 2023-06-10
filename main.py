626d0b73c6832aa2cda6d17873c01f773140b38f
mabeijing <58149278@qq.com>
2023-06-10 10:05:33 +0800

    日常

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from spider import Spider
 
 if __name__ == '__main__':
-    spider = Spider(only_update=False)
-    spider.gem_sticker_spu()
+    spider = Spider()
+    spider.gem_weapon_pistol_spu()
